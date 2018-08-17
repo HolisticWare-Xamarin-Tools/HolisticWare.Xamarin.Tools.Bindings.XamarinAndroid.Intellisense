f8b8c426d6af5ce4000052a6b1f2c3ae6cc8c060
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-08-17 19:09:15 +0200

    renamed project for testing binding lib

--- samples/Sample.XamarinAndroid.Bindings.Library/Properties/AssemblyInfo.cs
+++ samples/Sample.XamarinAndroid.Bindings.Library/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("XamarinAndroid.Bindings.Sample")]
+[assembly: AssemblyTitle("Sample.XamarinAndroid.Bindings.Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft / Xamarin Inc. ")]
